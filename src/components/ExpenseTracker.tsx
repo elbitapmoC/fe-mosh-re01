db21208660119f3ba1c98d60dbb0864340e54bd6
Bazell Partridge <elbitapmoC@users.noreply.github.com>
2023-04-11 14:37:07 -0400

    Works

--- src/components/ExpenseTracker.tsx
+++ src/components/ExpenseTracker.tsx
@@ -1,95 +1,146 @@
 import { useForm, SubmitHandler } from "react-hook-form";
+import ExpensesList from "./ExpensesList";
+import { useState } from "react";
 
 enum CategoriesEnum {
   groceries = "Groceries",
   utilities = "Utilities",
   entertainment = "Entertainment",
 }
+
 interface IFormInput {
+  id: number;
   description: string;
   amount: number;
-  category: CategoriesEnum;
+  categories: CategoriesEnum;
 }
 
 const ExpenseTracker = () => {
-  const { register, handleSubmit } = useForm<IFormInput>();
-  const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
+  const [expenses, setExpenses] = useState([
+    {
+      id: 1,
+      description: "PS5",
+      amount: 600,
+      categories: CategoriesEnum.entertainment,
+    },
+    {
+      id: 2,
+      description: "Nama J2 Juicer",
+      amount: 500,
+      categories: CategoriesEnum.utilities,
+    },
+    {
+      id: 3,
+      description: "Watermelon (seeded)",
+      amount: 7,
+      categories: CategoriesEnum.groceries,
+    },
+  ]);
+  const {
+    register,
+    formState: { errors },
+    reset,
+    handleSubmit,
+  } = useForm<IFormInput>();
+  const onSubmit: SubmitHandler<IFormInput> = (data) => {
+    reset();
+    setExpenses([...expenses, data]);
+  };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      {/* TODO: Add input validation-- 
-      Description should be at least 3 characters.
-      */}
-      <div className="mb-6">
-        <label
-          htmlFor="description"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          Description
-        </label>
-        <input
-          {...register("description")}
-          type="description"
-          id="description"
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="Playstation 5"
-          required
-        />
-      </div>
-      {/* TODO: Add input validation--
-      Amount required
-      */}
-      <div className="mb-6">
-        <label
-          htmlFor="amount"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          Amount
-        </label>
-        <input
-          {...register("amount")}
-          type="number"
-          id="amount"
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          required
-        />
-      </div>
-      {/* TODO: Add input validation 
-      Category required
-      */}
-      <div className="mb-8">
-        <label
-          htmlFor="categories"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          Category
-        </label>
-        <select
-          {...register("categories")}
-          id="categories"
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        >
-          <option disabled>Choose a category</option>
-          <option value={CategoriesEnum.groceries}>
-            {CategoriesEnum.groceries}
-          </option>
-          <option value={CategoriesEnum.utilities}>
-            {CategoriesEnum.utilities}
-          </option>
-          <option value={CategoriesEnum.entertainment}>
-            {CategoriesEnum.entertainment}
-          </option>
-        </select>
-      </div>
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="mb-6">
+          <label
+            htmlFor="description"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Description
+          </label>
+          <input
+            {...register("description", { required: true, minLength: 2 })}
+            type="description"
+            id="description"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Playstation 5"
+          />
+          {errors.description && (
+            <p className="text-red-500 mt-2">
+              Description should be at least 2 characters.
+            </p>
+          )}
+        </div>
 
-      {/* TODO: onSubmit-- clear input fields */}
-      <button
-        type="submit"
-        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-      >
-        Submit
-      </button>
-    </form>
+        <div className="mb-6">
+          <label
+            htmlFor="amount"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Amount
+          </label>
+          <input
+            min={1}
+            max={1000}
+            type="number"
+            id="amount"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            {...register("amount", {
+              valueAsNumber: true,
+              required: true,
+              min: 1,
+              max: 1000,
+            })}
+          />
+          {errors.amount && (
+            <p className="text-red-500 mt-2">
+              Amount is required & must be greater than 0.
+            </p>
+          )}
+        </div>
+
+        <div className="mb-8">
+          <label
+            htmlFor="categories"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Category
+          </label>
+          <select
+            id="categories"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            {...register("categories", { required: true })}
+          >
+            <option value="">Pick one</option>
+            <option value={CategoriesEnum.groceries}>
+              {CategoriesEnum.groceries}
+            </option>
+            <option value={CategoriesEnum.utilities}>
+              {CategoriesEnum.utilities}
+            </option>
+            <option value={CategoriesEnum.entertainment}>
+              {CategoriesEnum.entertainment}
+            </option>
+          </select>
+          {errors.categories && (
+            <p className="text-red-500 mt-2">Category required.</p>
+          )}
+        </div>
+
+        <button
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Submit
+        </button>
+      </form>
+
+      <ExpensesList
+        expenses={expenses}
+        onDelete={(id) => {
+          setExpenses(expenses.filter((e) => e.id != id));
+        }}
+      />
+    </>
   );
 };
 
---
+++ src/components/ExpensesList.tsx
@@ -0,0 +1,88 @@
+import { BsTrash } from "react-icons/bs";
+
+enum CategoriesEnum {
+  groceries = "Groceries",
+  utilities = "Utilities",
+  entertainment = "Entertainment",
+}
+
+interface Expense {
+  id: number;
+  description: string;
+  amount: number;
+  categories: CategoriesEnum;
+}
+
+interface Prop {
+  expenses: Expense[];
+  onDelete: (id: number) => void;
+}
+
+const ExpensesList = ({ expenses, onDelete }: Prop) => {
+  console.log(expenses);
+  return (
+    <div className="mt-20">
+      <div className="relative overflow-x-auto">
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Product description
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Cost
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Category
+              </th>
+
+              <th scope="col" className="px-6 py-3">
+                Actions
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {expenses.map(
+              ({ id, description, amount, categories }: Expense) => (
+                <tr
+                  className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                  key={id}
+                >
+                  <th
+                    scope="row"
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    {description}
+                  </th>
+                  <td className="px-6 py-4">{amount}</td>
+                  <td className="px-6 py-4">{categories}</td>
+                  <td className="px-6 py-4">
+                    <button
+                      className="flex align-center items-center bg-red-600 px-4 py-2"
+                      onClick={() => onDelete(id)}
+                    >
+                      <BsTrash size={15} color="white" />{" "}
+                      <span className="pl-2 text-white">Delete</span>
+                    </button>
+                  </td>
+                </tr>
+              )
+            )}
+          </tbody>
+          <tfoot className="text-xs uppercase bg-gray-800 text-white outline outline-offset-2 outline-1">
+            <tr>
+              <th className="px-6 py-3">Total:</th>
+              <td>
+                {expenses.reduce((acc, expenses) => expenses.amount + acc, 0)}
+              </td>
+              <td></td>
+              <td></td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default ExpensesList;
